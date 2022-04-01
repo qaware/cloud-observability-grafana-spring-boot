55df759aaba382217976744743904ee13ab88d58
Andreas Grub <andreas.grub@qaware.de>
2022-04-01 05:52:52 +0200

    Better logs for sleeping in /trigger-me endpoint

--- spring-boot-app/src/main/java/de/qaware/demo/cloudobservability/SpringBootDemoController.java
+++ spring-boot-app/src/main/java/de/qaware/demo/cloudobservability/SpringBootDemoController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.Duration;
+import java.time.Instant;
 
 @RestController
 public class SpringBootDemoController {
@@ -21,15 +22,17 @@ public class SpringBootDemoController {
     }
 
     private Duration sleepForRandomTime() {
-        var sleepDuration = Duration.ofMillis(Math.round(MAX_SLEEP_TIME.toMillis() * Math.random()));
         try {
-            LOGGER.info("Sleeping for {}", sleepDuration);
-            Thread.sleep(sleepDuration.toMillis());
-            LOGGER.info("Done sleeping...");
+            var minSleepDuration = Duration.ofMillis(Math.round(MAX_SLEEP_TIME.toMillis() * Math.random()));
+            LOGGER.info("Sleeping for at least {}", minSleepDuration);
+            var start = Instant.now();
+            Thread.sleep(minSleepDuration.toMillis());
+            Duration sleptFor = Duration.between(start, Instant.now());
+            LOGGER.info("Slept for {}", sleptFor);
+            return sleptFor;
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
             throw new IllegalStateException("Interrupted during sleep", e);
         }
-        return sleepDuration;
     }
 }
